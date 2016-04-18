e88561060dfdc4cf14044fae31e9daabcfa39bd0
NeoLinkin <wudong125125@163.com>
2016-04-18 17:38:40 +0800

    Create sort.py

---
+++ sort.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+def by_name(t):
+        return t[0]
+
+L = [('Bob', 75), ('Adam', 92), ('Bart', 66), ('Lisa', 88)]
+L2=sorted(L,key=by_name)
+print "by_name: ",L2
+
+def by_score(t):
+        return t[1]
+
+L2 = sorted(L,key=by_score,reverse=True)
+print "by_score: ",L2
