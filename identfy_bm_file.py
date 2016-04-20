74a02952bf3f76dbe4f892c55349fd1587869d23
NeoLinkin <wudong125125@163.com>
2016-04-20 17:03:52 +0800

    Update identfy_bm_file.py

--- identfy_bm_file.py
+++ identfy_bm_file.py
@@ -13,3 +13,5 @@ if ss[0] == 'B' and ss[1] == 'M':
         print 'This is a bmp file! It has ',ss[6],'*', ss[7],', It is color is ',ss[9],'.'
 else:
         print 'This is not a bmp file!'
+
+f.close()
